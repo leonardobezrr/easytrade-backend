3cdf66b2fc35a094470969dd407bb0852655d248
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-12-02 10:36:38 -0300

    feat: Adição de struct Produto_Venda

--- api/Models/models.go
+++ api/Models/models.go
@@ -3,16 +3,23 @@ package models
 // esse diretório irá armazenas as structs
 
 type Usuarios struct {
-	ID    string `json:"string"`
+	ID    string `json:"id"`
 	Nome  string `json:"nome"`
 	Email string `json:"email"`
 	Senha string `json:"senha"`
 }
 
 type Produtos struct {
-	ID string `json:"string"`
+	ID string `json:"id"`
 	Nome  string `json:"nome"`
 	Descricao string `json:"descricao"`
 	Preco float64 `json:"preco"`
 	Qtd_estoque int64 `json:"qtd_estoque"`
+}
+
+type Produto_Venda struct {
+	IDProduto string `json:"idproduto"`
+	IDVenda string `json:"idvenda"`
+	Valor_unitario float64 `json:"valor_unitario"`
+	Quantidade int64 `json:"quantidade"`
 }
