789eaaa866955f567b4cecd86f2dec847039d31d
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-12-02 10:05:46 -0300

    feat: Adição de structs de usuários

--- api/Models/models.go
+++ api/Models/models.go
@@ -1,3 +1,10 @@
 package models
 
 // esse diretório irá armazenas as structs
+
+type Usuarios struct {
+	ID    string `json:"string"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+	Senha string `json:"senha"`
+}
