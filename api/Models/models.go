77d11d8c52960aae53acadcd3932b4cc138133fa
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-12-02 10:20:27 -0300

    feat: Adição do struct Produtos

--- api/Models/models.go
+++ api/Models/models.go
@@ -8,3 +8,11 @@ type Usuarios struct {
 	Email string `json:"email"`
 	Senha string `json:"senha"`
 }
+
+type Produtos struct {
+	ID string `json:"string"`
+	Nome  string `json:"nome"`
+	Descricao string `json:"descricao"`
+	Preco float64 `json:"preco"`
+	Qtd_estoque int64 `json:"qtd_estoque"`
+}
