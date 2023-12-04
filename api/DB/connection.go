47d09e82957f8102f81d85d0d8fce452ceadf3f7
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-04 17:51:34 -0300

    feat: adicionando arquivo .env para proteção de dados

--- api/DB/connection.go
+++ api/DB/connection.go
@@ -1,22 +1,33 @@
+// connection.go
 package db
 
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func OpenConnection() (*sql.DB, error) {
+	// Leitura das variáveis de ambiente
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
 
-	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", "localhost", "5432", "user_easytrade", "1122", "easytrade")
+	// Construção da string de conexão
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
+	// Abertura da conexão
 	conn, err := sql.Open("postgres", sc)
 
 	if err != nil {
 		panic(err)
 	}
 
+	// Verificação da conexão
 	err = conn.Ping()
 
 	return conn, err
