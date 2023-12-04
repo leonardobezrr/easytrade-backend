79af675ac61984a6ac9c49cc11cf93558d22be24
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-04 17:16:15 -0300

    feat: correção de inserção de produto no banco de dados - salvando id do usuário

--- api/Repository/repository.go
+++ api/Repository/repository.go
@@ -3,9 +3,9 @@ package repository
 import (
 	db "easytrady-backend/api/DB"
 	models "easytrady-backend/api/Models"
+	"fmt"
 )
 
-// esse repostitório irá lidar com as consultas ao banco de dados
 func InsertUser(usuario models.Usuarios) (id int, err error) {
 
 	conn, err := db.OpenConnection()
@@ -20,22 +20,27 @@ func InsertUser(usuario models.Usuarios) (id int, err error) {
 
 	err = conn.QueryRow(sql, usuario.Nome, usuario.Email, usuario.Senha).Scan(&id)
 
+	if err != nil {
+		fmt.Println("Erro ao inserir produto no banco de dados:", err)
+	}
+
 	return
 }
 
 func InsertProduto(produto models.Produtos) (id int, err error) {
-
 	conn, err := db.OpenConnection()
-
 	if err != nil {
 		return
 	}
-
 	defer conn.Close()
 
-	sql := `INSERT INTO produtos (nome, descricao, preco, qtd_estoque) VALUES ($1, $2, $3, $4) RETURNING id`
+	sql := `INSERT INTO produtos (nome, descricao, preco, qtd_estoque, id_usuario) VALUES ($1, $2, $3, $4, $5) RETURNING id`
 
-	err = conn.QueryRow(sql, produto.Nome, produto.Descricao, produto.Preco, produto.Qtd_estoque).Scan(&id)
+	err = conn.QueryRow(sql, produto.Nome, produto.Descricao, produto.Preco, produto.Qtd_estoque, produto.Usuarios).Scan(&id)
+
+	if err != nil {
+		fmt.Println("Erro ao inserir produto no banco de dados:", err)
+	}
 
 	return
 }
