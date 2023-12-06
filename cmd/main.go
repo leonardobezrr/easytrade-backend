c27f905cf447482900b3ea49f0b245127cc777e9
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-12-06 10:54:16 -0300

    feat: correções

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"easytrady-backend/api"
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Erro ao carregar o arquivo .env", err)
-	}
 
 	e := echo.New()
 
