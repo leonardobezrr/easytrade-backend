e11cbd93d47455663d57e172960628f1532ec019
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-13 14:53:16 -0300

    feat: puxando produtos das vendas

--- api/Repository/repositoryvendas.go
+++ api/Repository/repositoryvendas.go
@@ -18,7 +18,7 @@ func InsertVenda(venda models.Venda) (id int, err error) {
 	defer conn.Close()
 
 	sql := `INSERT INTO VENDAS (data_venda, valor_venda, id_usuario) VALUES ($1, $2, $3) RETURNING id`
-	err = conn.QueryRow(sql, venda.Data_venda, venda.Valor_venda, venda.Usuarios).Scan(&id)
+	err = conn.QueryRow(sql, venda.Data_venda, venda.Valor_venda, venda.Usuarios, venda.Produtos).Scan(&id)
 
 	if err != nil {
 		fmt.Println("Erro ao inserir venda no banco de dados: ", err)
@@ -36,6 +36,53 @@ func InsertVenda(venda models.Venda) (id int, err error) {
 	}
 
 	return id, nil
+}
+
+func GetProdutoByVendaId(vendaID string) ([]models.Produto_Venda, error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	// Construa a consulta SQL com base no parâmetro vendaID
+	query := `SELECT produtos_venda.id, produtos_venda.id_produto, vendas.id, produtos_venda.valor_unitario, produtos_venda.quantidade FROM vendas JOIN produtos_venda ON vendas.id = produtos_venda.id_venda WHERE vendas.id = $1;`
+
+	rows, err := conn.Query(query, vendaID)
+	if err != nil {
+		log.Println("Erro ao executar a consulta:", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Crie um slice para armazenar os resultados
+	var produtos []models.Produto_Venda
+
+	// Itere sobre as linhas do resultado
+	for rows.Next() {
+		var produto models.Produto_Venda
+		err := rows.Scan(
+			&produto.ID,
+			&produto.IDProduto,
+			&produto.IDVenda,
+			&produto.Valor_unitario,
+			&produto.Quantidade,
+		)
+		if err != nil {
+			log.Println("Erro ao escanear linha:", err)
+			return nil, err
+		}
+		produtos = append(produtos, produto)
+	}
+
+	// Verifique se houve algum erro durante a iteração
+	if err := rows.Err(); err != nil {
+		log.Println("Erro durante a iteração das linhas:", err)
+		return nil, err
+	}
+
+	// Retorne os resultados
+	return produtos, nil
 }
 
 func GetVendasByUsuarioID(usuarioID string) ([]models.Venda, error) {
